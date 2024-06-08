0d3eff120ae6dcd3cbb83b1440ce098e0b186618
Harsh-9000 <harshchaudharydb@gmail.com>
2024-06-09 02:04:55 +0530

    Fixed Search Bar in sign in and register

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
@@ -8,13 +9,25 @@ interface Props {
 }
 
 const Layout = ({ children }: Props) => {
+  const location = useLocation()
+  const { pathname } = location
+
+  const showSearchBar = ![
+    '/register',
+    '/sign-in',
+    '/my-bookings',
+    '/my-hotels',
+  ].includes(pathname)
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
-      <div className='container mx-auto'>
-        <SearchBar />
-      </div>
+      {showSearchBar && (
+        <div className="container mx-auto">
+          <SearchBar />
+        </div>
+      )}
       <div className="container mx-auto py-10 flex-1">{children}</div>
       <Footer />
     </div>
